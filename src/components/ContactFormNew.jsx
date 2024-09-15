eed6bf4d63f18cb20b76559e852dfdb8a09dae75
Mohamed Kamran <164040347+mdkamran04@users.noreply.github.com>
2024-09-15 09:41:51 +0530

    fixed mail

--- src/components/ContactFormNew.jsx
+++ src/components/ContactFormNew.jsx
@@ -45,8 +45,8 @@ const ContactFormNew = () => {
             className="h-8 w-8 mx-auto mb-4"
           />
           <h4 className="font-semibold text-blue-700">EMAIL</h4>
-          <p className="text-sm text-gray-600 mt-2">dnx@gmail.com</p>
-          <p className="text-sm text-gray-600">medin@gmail.com</p>
+         
+          <p className="text-sm text-gray-600">medibridge@gmail.com</p>
         </div>
 
         {/* Working Hours */}
